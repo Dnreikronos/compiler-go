eae14b741de82074b98960f246e719d186053358
Dnreikronos <dnreikronos2@gmail.com>
2025-01-30 20:31:57 -0300

    feature: assign variables in TokenType

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,3 +1,11 @@
 package lexer
 
 type TokenType string
+const (
+	Number   TokenType = "Number"
+	Plus     TokenType = "+"
+	Minus    TokenType = "-"
+	Multiply TokenType = "*"
+	Divide   TokenType = "/"
+	EOF      TokenType = "EOF"
+)
