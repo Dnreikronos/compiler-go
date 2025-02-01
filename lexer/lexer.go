a9391d2474df511cfe1151f3eb2f4c6eebb8f338
Dnreikronos <dnreikronos2@gmail.com>
2025-02-01 20:17:15 -0300

    feature: create lexer functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -20,3 +20,35 @@ type Token struct {
 	Type  TokenType
 	Value string
 }
+
+func Lex(input string) []Token {
+	var tokens []Token
+	r := strings.NewReader(input)
+
+	for {
+		ch, _, err := r.ReadRune()
+		if err != nil { // End of input
+			tokens = append(tokens, Token{EOF, ""})
+			break
+		}
+
+		switch {
+		case unicode.IsSpace(ch):
+			continue // Skip whitespace
+		case unicode.IsDigit(ch):
+			r.UnreadRune()
+			num := readNumber(r)
+			tokens = append(tokens, Token{Number, num})
+		case ch == '+':
+			tokens = append(tokens, Token{Plus, "+"})
+		case ch == '-':
+			tokens = append(tokens, Token{Minus, "-"})
+		case ch == '*':
+			tokens = append(tokens, Token{Multiply, "*"})
+		case ch == '/':
+			tokens = append(tokens, Token{Divide, "/"})
+		}
+	}
+
+	return tokens
+}
