074787177577de14bd7d86b58ae117c64518fc3d
Dnreikronos <dnreikronos2@gmail.com>
2025-02-07 12:20:35 -0300

    feature: create the traverse function to the compiler

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -7,3 +7,30 @@ func Generate(expr parser.Expr) string {
 	output += "		syscall\n"
 	return output
 }
+func traverse(expr parser.Expr) string {
+	switch e := expr.(type) {
+	case parser.NumberExpr:
+		return fmt.Sprintf("    mov rax, %s\n", e.Value)
+	case parser.BinaryExpr:
+		left := traverse(e.Left)
+		right := traverse(e.Right)
+		code := left +
+			"    push rax\n" +
+			right +
+			"    pop rbx\n"
+		switch e.Op {
+		case "+":
+			code += "    add rax, rbx\n"
+		case "-":
+			code += "    sub rax, rbx\n"
+		case "*":
+			code += "    imul rax, rbx\n"
+		case "/":
+			code += "    cqo\n"
+			code += "    idiv rbx\n"
+		}
+		return code
+	default:
+		panic("Unsupported expression")
+	}
+}
