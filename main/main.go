d990f44e8e67a7f8ca4bf6359a39a6121212411a
Dnreikronos <dnreikronos2@gmail.com>
2025-02-09 08:43:23 -0300

    feature: create main function for run the application

--- main/main.go
+++ main/main.go
@@ -9,3 +9,22 @@ import (
 	"github.com/Dnreikronos/compiler-go/lexer"
 	"github.com/Dnreikronos/compiler-go/parser"
 )
+func main() {
+	input := "1 + 2 * 3"
+
+	tokens := lexer.Lex(input)
+
+	p := parser.NewParser(tokens)
+	ast := p.Parse()
+
+	asm := codegen.Generate(ast)
+
+	os.WriteFile("output.s", []byte(asm), 0644)
+
+	cmd := exec.Command("nasm", "-felf64", "output.s")
+	cmd.Run()
+	cmd = exec.Command("ld", "-o", "output", "output.o")
+	cmd.Run()
+
+	fmt.Println("Compilation successful. Run './output'")
+}
