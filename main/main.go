ab8b1a8eec733626c5e6fcb4204f8a46862815ea
Dnreikronos <dnreikronos2@gmail.com>
2025-02-09 08:43:05 -0300

    feature:  add imports to the main package

---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/Dnreikronos/compiler-go/codegen"
+	"github.com/Dnreikronos/compiler-go/lexer"
+	"github.com/Dnreikronos/compiler-go/parser"
+)
